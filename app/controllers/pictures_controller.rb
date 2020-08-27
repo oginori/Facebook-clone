0e42f44d7f994eff72c0a3f9cb9a4964c32a1f20
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-28 08:03:42 +0900

    編集機能を作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,6 +16,19 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "投稿を編集しました"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def picture_params
