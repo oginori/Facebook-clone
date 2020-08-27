0fc85f87b71a1e890f80a3221ba46d6828402d3b
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-28 07:54:34 +0900

    詳細画面を作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,24 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to pictures_path
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content)
   end
 end
