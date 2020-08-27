95d521d7468cd67fca84c3ee2d74784c90a735da
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-28 08:27:48 +0900

    削除機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
   def index
     @pictures = Picture.all
   end
@@ -13,15 +14,12 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
   end
 
   def edit
-    @picture = Picture.find(params[:id])
   end
 
   def update
-    @picture = Picture.find(params[:id])
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "投稿を編集しました"
     else
@@ -29,9 +27,18 @@ class PicturesController < ApplicationController
     end
   end
 
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "投稿を削除しました"
+  end
+
   private
 
   def picture_params
     params.require(:picture).permit(:content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
