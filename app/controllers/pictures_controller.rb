95b6e6dfbf67e6f87b49c08146c0865ca81ad821
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-30 18:22:18 +0900

    アソシエーションを設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,7 +9,7 @@ class PicturesController < ApplicationController
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
     if params[:back]
       render :new
     else
@@ -41,7 +41,8 @@ class PicturesController < ApplicationController
   end
 
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+    @picture.user_id = current_user.id
     render :new if @picture.invalid?
   end
 
